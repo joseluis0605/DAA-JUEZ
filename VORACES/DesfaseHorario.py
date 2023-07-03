56b4862c361bc152f7ac912360e599080bcad239
José Luis <joseluis06052001@gmail.com>
2023-07-03 19:19:25 +0200

    nuevos ejercicios juez DAA

--- VORACES/DesfaseHorario.py
+++ VORACES/DesfaseHorario.py
@@ -1,23 +1,21 @@
-def horario(data):
-    data.sort(key=lambda a: a[2])
-    n = len(data)
+def horario(lista):
+    tam = len(lista)
     tareas = 0
-    act_time = -1
-    for i in range(n):
-        if data[i][1] > act_time:
-            act_time = data[i][2]
+    tiempo = -1
+    for i in range(tam):
+        if lista[i][1] > tiempo:
+            tiempo = lista[i][2]
             tareas += 1
         i += 1
-    return tareas
+    print(tareas)
 
+### main program
+numeroTareas = int(input())
+valores=[]
 
-n_tareas = int(input())
-
-data = []
-
-for _ in range(n_tareas):
-    nombre, init, fin = input().split()
-    data.append((str(nombre), int(init), int(fin)))
-
-print(horario(data))
+for _ in range(numeroTareas):
+    nombre, inicio, fin = input().split()
+    valores.append((str(nombre), int(inicio), int(fin)))
+valores.sort(key=lambda x: x[2])
+horario(valores)
 
